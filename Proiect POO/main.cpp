43ab850ce6d703db52c5c8f6287ab284ed479b3e
Amo <emperoreddy@gmail.com>
2023-01-07 20:32:13 +0200

    added inchidereCont

--- Proiect POO/main.cpp
+++ Proiect POO/main.cpp
@@ -44,6 +44,7 @@ public:
 	void incrementareNumarConturi();
 	bool numarContEsteValid(int numarCont);
 	void citireFisier();
+	void inchidereCont(int cont);
 
 
 	~Cont() {
@@ -306,10 +307,90 @@ void Cont::creareCont() {
 	}
 
 
+void Cont::inchidereCont(int cont) {
+	int* temp = new int[this->index - 2]; 
+	temp[0] = this->valori[0];
+	bool gasit = false;
+	
+	// copiere valori bune in temp
+	for (int i = 1; i < this->index; i += 2) {
+
+		if (this->valori[i] != cont) {
+			temp[i] = this->valori[i];
+			temp[i + 1] = this->valori[i + 1];
+		}
+		else gasit = true;
+	}
+
+	// daca nu exista, iesim din functie
+	if (!gasit) return;
+
+	this->valori = temp;
+
+
+	// MODIFICARE FISIER
+
+	ifstream filein("conturi.txt");
+	ofstream fileout("conturiNew.txt");
+	int numarContInt = preluareNumarDeConturiFisier();
+	// prima valoare este numarul de conturi, o incrementez 
+	this->valori[0] = numarContInt - 1;
+
+	if (!filein || !fileout) {
+		cout << "Eroare deschidere" << endl;
+		return;
+	}
+	else {
+		fileout << this->valori[0] << endl << endl;
+		for (int i = 1; i < index; i++)
+		{
+			if (this->valori[i] < 0) continue;
+			fileout << this->valori[i];
+			fileout << " ";
+			if (i % 2 == 0) fileout << endl << endl;
+		}
+	}
+
+	filein.close();
+	fileout.close();
+
+
+
+	//stergere si redenumire fisier nou cu cel vechi
+	int remove_result = remove("conturi.txt");
+	if (remove_result != 0) {
+		// An error occurred
+		char* error_message = strerror(errno);
+		printf("Error deleting file: %s\n", error_message);
+	}
+	else {
+		printf("File deleted succesfully");
+	}
+
+
+
+
+	int rename_result = rename("conturiNew.txt", "conturi.txt");
+	if (rename_result != 0) {
+		char* error_message = strerror(errno);
+		printf("Error renaming file: %s\n", error_message);
+	}
+	else {
+		printf("File renamed succesfully");
+	}
+
+}
+
+
+
 int main() {
 	Cont cont;
 	 
 	cont.citireFisier();
 	cont.creareCont();
-	//cont.getValori();
+	cont.creareCont();
+	cont.getValori();
+	cout << endl << endl;
+	cont.inchidereCont(3);
+
 }
