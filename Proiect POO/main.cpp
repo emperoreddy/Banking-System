3d6cdad4ce22ee1919ea8e27f4342743c0f3c752
Amo <emperoreddy@gmail.com>
2023-01-14 23:24:19 +0200

    implemented deposit

--- Proiect POO/main.cpp
+++ Proiect POO/main.cpp
@@ -37,7 +37,7 @@ public:
 	}
 
 
-	int preluareDateDinFisier(int numarCont);
+	int gasireValoareCont(int numarCont);
 	int preluareNumarDeConturiFisier();
 	void creareCont();
 	bool fisierGol();
@@ -45,6 +45,8 @@ public:
 	bool numarContEsteValid(int numarCont);
 	void citireFisier();
 	void inchidereCont(int cont);
+	void depunereBani(int cont, int suma);
+	void stergereFisiere();
 
 
 	~Cont() {
@@ -127,7 +129,7 @@ bool Cont::fisierGol() {
 	}
 }*/
 
-int Cont::preluareDateDinFisier(int numarCont) {
+int Cont::gasireValoareCont(int numarCont) {
 	// cauta si gaseste valoarea unui cont (balanta)
 	ifstream fisierConturi;
 	map<int, int> conturi;
@@ -253,28 +255,8 @@ void Cont::incrementareNumarConturi() {
 
 
 
-	 //stergere si redenumire fisier nou cu cel vechi
-	int remove_result = remove("conturi.txt");	 
-	if (remove_result != 0) {
-    // An error occurred
-    char* error_message = strerror(errno);
-    //printf("Error deleting file: %s\n", error_message);
-	}
-	//else {
-	//	printf("File deleted succesfully");
-	//}
+	stergereFisiere();
 
-
-	
-	
-	int rename_result = rename("conturiNew.txt", "conturi.txt");
-	if (rename_result != 0) {
-		char* error_message = strerror(errno);
-		//printf("Error renaming file: %s\n", error_message);
-	}
-	//else {
-	//	printf("File renamed succesfully");
-	//}
 }
 
 
@@ -344,7 +326,7 @@ void Cont::inchidereCont(int cont) {
 		fileout << this->valori[0] << endl << endl;
 		for (int i = 1; i < index; i++)
 		{
-			if (this->valori[i] < 0) continue;
+			if (this->valori[i] < 0 || this->valori[i] > 100000000) continue;
 			fileout << this->valori[i];
 			fileout << " ";
 			if (i % 2 == 0) fileout << endl << endl;
@@ -355,6 +337,67 @@ void Cont::inchidereCont(int cont) {
 	fileout.close();
 
 
+
+	stergereFisiere();
+}
+
+
+void Cont::depunereBani(int cont, int suma) {
+
+	int* temp = new int[this->index];
+	temp[0] = this->valori[0];
+	bool gasit = false;
+
+	// copiere valori bune in temp
+	for (int i = 1; i < this->index; i += 2) {
+
+		if (this->valori[i] != cont) {
+			temp[i] = this->valori[i];
+			temp[i + 1] = this->valori[i + 1];
+		}
+		else { 
+			gasit = true; 
+			temp[i] = this->valori[i];
+			temp[i + 1] = this->valori[i + 1] + suma;
+		}
+	}
+
+	// daca nu exista, iesim din functie
+	if (!gasit) return;
+
+	this->valori = temp;
+
+
+	// MODIFICARE FISIER
+
+	ifstream filein("conturi.txt");
+	ofstream fileout("conturiNew.txt");
+
+	if (!filein || !fileout) {
+		cout << "Eroare deschidere" << endl;
+		return;
+	}
+	else {
+		fileout << this->valori[0] << endl << endl;
+		for (int i = 1; i < index; i++)
+		{
+			if (this->valori[i] < 0 || this->valori[i] > 100000000) continue;
+			fileout << this->valori[i];
+			fileout << " ";
+			if (i % 2 == 0) fileout << endl << endl;
+		}
+	}
+
+	filein.close();
+	fileout.close();
+
+
+
+	stergereFisiere();
+}
+
+
+void Cont::stergereFisiere() {
 
 	//stergere si redenumire fisier nou cu cel vechi
 	int remove_result = remove("conturi.txt");
@@ -378,7 +421,6 @@ void Cont::inchidereCont(int cont) {
 	//else {
 	//	printf("File renamed succesfully");
 	//}
-
 }
 
 
@@ -388,9 +430,14 @@ int main() {
 	 
 	cont.citireFisier();
 	cont.creareCont();
-	cont.creareCont();
-	cont.getValori();
+	//cont.getValori();
 	cout << endl << endl;
-	cont.inchidereCont(3);
+	cont.inchidereCont(2);
+
+	cont.depunereBani(4, 1);
+
+	cout << "valoarea contului 4 e " << cont.gasireValoareCont(4);
+
+
 
 }
